22696264497faec3c902143ede27bea4cfbdf8ff
tanvirhridoy <ikramulhasantanvir@gmail.com>
2025-05-29 20:27:07 +0600

    payment-key ignored

--- lib/pregentaition/screens/home/booked_now_screen/utils/payment_keys.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-class PaymentKeys {
- static String publishAbleKey = dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
- static String secretKey = dotenv.env['STRIPE_SECRET_KEY']!;
-}
