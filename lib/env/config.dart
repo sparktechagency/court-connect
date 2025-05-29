3d7714db845091be67c146d68c38ff19432f2195
tanvirhridoy <ikramulhasantanvir@gmail.com>
2025-05-29 22:29:55 +0600

    Ignore fvm folder

---
+++ lib/env/config.dart
@@ -0,0 +1,13 @@
+class Config {
+  static String _publishableKey = 'pk_test_51RE5CTBAXOk4i1N0dYYtBsgGU6XH1EstVgeHWzodeWvXGyDZyjQVkHlPqIdgjukTl4AxCKfjzCEeCYFRXkivBlXQ00Ve6spk6T';
+  static String _secretKey = 'sk_test_51RE5CTBAXOk4i1N0R115qs3PWDQOpHZM8ynCK2dWhvgF4d1iE6UmpomEvXkLBpkCXDK4NlilZffRYRhRlrgU56XB008quQlIBa';
+
+
+  static String get publishableKey => _publishableKey;
+  static String get secretKey => _secretKey;
+}
+
+
+
+
+
